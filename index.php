2951f528163e224beb013497ddf7e66d2fde2c82
Soumya-Joy <43504113+Soumya-Joy@users.noreply.github.com>
2019-10-14 04:58:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -314,62 +314,38 @@ if(isset($_POST['submit']))
 
   <div class="container-fluid text-center" id="new">
       <div class="row">
+         <?php
+            $query = "SELECT * from book ORDER BY RAND()";
+            $result = mysqli_query($con, $query);
+            if(mysqli_num_rows($result) > 0)
+            {
+                while($row= mysqli_fetch_array($result)){
+          ?>
           <div class="col-sm-6 col-md-3 col-lg-3">
-           <a href="description.php?ID=NEW-1&category=new">
-              <div class="book-block">
-                  <div class="tag">New</div>
-                  <div class="tag-side"><img src="img/tag.png"></div>
-                  <img class="book block-center img-responsive" src="img/new/1.jpg">
-                  <hr>
-                  Like A Love Song <br>
-                  Rs 113  &nbsp
-                  <span style="text-decoration:line-through;color:#828282;"> 175 </span>
-                  <span class="label label-warning">35%</span>
-              </div>
-            </a>
+              <a href="">
+                  <div class="book-block">
+                      <img class="book block-center img-responsive" src="img/new/1.jpg">
+                      <hr>
+                      <?php echo $row['title']; ?><br>
+                      <?php echo $row['oprice']; ?>  &nbsp
+                      <span style="text-decoration:line-through;color:#828282;"> <?php echo $row['aprice']; ?> </span>
+                      <span class="label label-warning">
+                          <?php 
+                                $per= 100-(($row['oprice']*100)/$row['aprice']);
+                                echo round($per)."%" ;
+                          ?>
+                      </span>
+                  </div>
+              </a>
           </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-           <a href="description.php?ID=NEW-2&category=new">
-              <div class="book-block">
-                  <div class="tag">New</div>
-                  <div class="tag-side"><img src="img/tag.png"></div>
-                  <img class="block-center img-responsive" src="img/new/2.jpg">
-                  <hr>
-                  General Knowledge 2017  <br>
-                  Rs 68 &nbsp
-                  <span style="text-decoration:line-through;color:#828282;"> 120 </span>
-                  <span class="label label-warning">43%</span>
-              </div>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-           <a href="description.php?ID=NEW-3&category=new">
-              <div class="book-block">
-                  <div class="tag">New</div>
-                  <div class="tag-side"><img src="img/tag.png"></div>
-                  <img class="block-center img-responsive" src="img/new/3.png">
-                  <hr>
-                  Indian Family Bussiness Mantras <br>
-                  Rs 400 &nbsp
-                  <span style="text-decoration:line-through;color:#828282;"> 595 </span>
-                  <span class="label label-warning">33%</span>
-              </div>
-            </a>
-          </div>
-          <div class="col-sm-6 col-md-3 col-lg-3">
-           <a href="description.php?ID=NEW-4&category=new">
-              <div class="book-block">
-                  <div class="tag">New</div>
-                  <div class="tag-side"><img src="img/tag.png"></div>
-                  <img class="block-center img-responsive" src="img/new/4.jpg">
-                  <hr>
-                  Kiran s SSC Mathematics Chapterwise Solutions <br>
-                  Rs 289 &nbsp
-                  <span style="text-decoration:line-through;color:#828282;"> 435 </span>
-                  <span class="label label-warning">33%</span>
-              </div>
-            </a>
-          </div>
+          <?php
+                }
+            }
+          ?>
+         
+         
+         
+          
       </div>
   </div>
 
